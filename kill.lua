534f7d3b4eff1fac59088fb613f23d2b94c1b90b
RBXHacker59 <154455173+RBXHacker59@users.noreply.github.com>
2024-01-02 09:25:34 -0300

    Update kill.lua

--- kill.lua
+++ kill.lua
@@ -1,1 +1,10 @@
+local kill = script.Parent
 
+local function onTouch(brick)
+  if brick.Parent:FindFirstChild("Humanoid") then
+      brick.Parent.Humanoid.Health = 0
+  end
+end
+
+
+kill.Touched:Connect(onTouch)
